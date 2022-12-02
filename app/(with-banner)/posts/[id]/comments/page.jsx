211dcd2bd0a3012f2166260d936ff950aefe47f8
yebrai <ivansevv@gmail.com>
2022-12-02 19:34:24 +0100

    done

--- app/(with-banner)/posts/[id]/comments/page.jsx
+++ app/(with-banner)/posts/[id]/comments/page.jsx
@@ -1,8 +1,10 @@
 import style from './Comments.module.css'
 
+import Image from 'next/image';
+
 const fetchComments = async (id) => {
     await new Promise(resolve=> setTimeout(resolve, 3000))
-    throw new Error('Error loading comments')
+   // throw new Error('Error loading comments')
 
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
@@ -20,6 +22,7 @@ export default async function Post({ params }) {
   <ul className={style.list}>
     {comments.map(comment => (
         <li key={comment.id}>
+        <Image width='50' height='50' alt={comment.name} src={`https://avatars.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`}/>
             <h2>{comment.name}</h2>
             <p>{comment.body}</p>
         </li>
--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,8 @@ import { Navigation } from "../components/Navigation";
 import {Space_Grotesk} from '@next/font/google'
 
 const font = Space_Grotesk({
-  weight: ['400', '700']
+  weight: ['400', '700'],
+  subsets:['latin']
 })
 
 export default function RootLayout({ children }) {
