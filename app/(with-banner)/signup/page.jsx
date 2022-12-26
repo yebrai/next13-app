e310e6bf0dd6dfe15e17974f85a3af0fc16f718a
yebrai <ivansevv@gmail.com>
2022-12-26 15:00:55 +0100

    react-hook-form done

--- app/(with-banner)/signup/page.jsx
+++ app/(with-banner)/signup/page.jsx
@@ -1,3 +1,49 @@
-export default function Signup () {
+'use client'
 
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function Signup () {
+  const [userData, setUserData] = useState()
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+  const onSubmit = (data) => {
+    setUserData(data)
+    console.log(userData)
+  }
+  return (
+    <>
+      <h4>Here we will test React-hook-form</h4>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <br />
+        <input {...register('name')} />
+        <br />
+        <label>Surname</label>
+        <br />
+        <input {...register('surname')} />
+        <br />
+        <label>Email</label>
+        <br />
+        <input {...register('email')} />
+        <br />
+        <label>Password</label>
+        <br />
+        <input {...register('password')} />
+        <br />
+        <input type='submit' />
+
+      </form>
+      <h5>This will be sended</h5>
+      {userData &&
+      (
+        <>
+          <p>Name: {userData.name}</p>
+          <p>Surname: {userData.surname}</p>
+          <p>Email: {userData.email}</p>
+          <p>Password: {userData.password}</p>
+        </>
+      )}
+    </>
+  )
 }
